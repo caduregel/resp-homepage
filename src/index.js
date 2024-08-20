7e828f4b2aec05c2f780c74dd048fc5bdcab7b49
liam <liam@nehoray.net>
2024-08-20 18:34:24 +0200

    End stage of the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './style.css'
+import './header.css'
+import './main_title.css'
+import './workshop.css'
+import './contact.css'
+
+let clicked = false
+document.querySelector('.dropbtn').addEventListener('click', () => {
+    if (clicked === false) {
+        clicked = true
+        document.querySelector("#myDropdown").classList.toggle("show");
+    } else {
+        clicked = false
+        document.querySelector("#myDropdown").classList.toggle("show");
+    }
+})
