9ef23bf9d83126b6d9a7f5a8db36ae31efa61fc8
liam <liam@nehoray.net>
2024-08-25 16:39:02 +0200

    Updated to clients liking:

--- src/index.js
+++ src/index.js
@@ -13,3 +13,44 @@ document.querySelector('.dropbtn').addEventListener('click', () => {
         document.querySelector("#myDropdown").classList.toggle("show");
     }
 })
+
+// $(window).scroll(function(e){ 
+//     var el = $('.boxed'); 
+//     var isPositionFixed = (el.css('position') == 'fixed');
+//     console.log(el)
+//     if ($(this).scrollTop() > 200 && !isPositionFixed){ 
+//       $el.css({'position': 'fixed', 'top': '0px'}); 
+//     }
+//     if ($(this).scrollTop() < 200 && isPositionFixed){
+//       $el.css({'position': 'static', 'top': '0px'}); 
+//     } 
+//   });
+let hasReachedTarget = false;
+window.addEventListener('scroll', function () {
+
+    // Get the current scroll position
+    const boxed = document.querySelector(".boxed")
+    const targetPosition = 220; // Target scroll position
+    const currentPosition = window.scrollY || window.pageYOffset;
+
+    if (window.innerWidth >= 1667) {
+
+        if (currentPosition >= targetPosition && !hasReachedTarget) {
+            // User scrolled down past the target position
+            hasReachedTarget = true;
+            console.l
+            // Call the event for scrolling down
+            boxed.classList.add('fixed')
+            boxed.style.margin = '30px 30px 30px 71.5%'
+        } else if (currentPosition < targetPosition && hasReachedTarget) {
+            // User scrolled back up above the target position
+            hasReachedTarget = false;
+            // Call the event for scrolling up
+            boxed.style.margin = '30px'
+
+            boxed.classList.remove('fixed')
+        }
+        window.removeEventListener('scroll', arguments.callee);
+    }
+
+})
